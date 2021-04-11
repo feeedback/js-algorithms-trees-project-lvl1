add12beb82679da314032dbcbc3bfc8dcae3e770
A. Rzhanitsyn <risdeep@gmail.com>
2021-04-12 02:13:01 +0500

    refactor build routes trie

--- src/index.js
+++ src/index.js
@@ -1,70 +1,83 @@
+const isThisParam = (segment) => segment.startsWith(':');
+
+const recursiveBuild = (route, routeSegments, currentNode) => {
+  const [word, ...tailSegments] = routeSegments;
+
+  const isParam = isThisParam(word);
+
+  const nodeKey = isParam ? '*' : word;
+  const node = { ...(currentNode[nodeKey] ?? {}) };
+
+  if (isParam) {
+    node.paramName = word.slice(1);
+  }
+
+  if (tailSegments.length === 0) {
+    return { [nodeKey]: { ...node, end: true, route } };
+  }
+
+  return { [nodeKey]: { ...node, ...recursiveBuild(route, tailSegments, node) } };
+};
+
 const generateTrie = (routes) => {
-  const trieRoutePart = {};
+  let trieRoutePart = {};
 
   for (const route of routes) {
     const routeSegments = route.path.split('/');
-    const lastIndex = routeSegments.length - 1;
-    let currentNode = trieRoutePart;
+    trieRoutePart = recursiveBuild(route, routeSegments, trieRoutePart);
+  }
+  return trieRoutePart;
+};
 
-    for (let i = 0; i < routeSegments.length; i += 1) {
-      const word = routeSegments[i];
-      const isThisParam = word.startsWith(':');
+const getParamsByTemplate = (path, route) => {
+  const pathSegments = path.split('/');
+  const routeSegments = route.split('/');
 
-      const nodeKey = isThisParam ? '*' : word;
-      if (!currentNode[nodeKey]) {
-        currentNode[nodeKey] = {};
-      }
-      const node = currentNode[nodeKey];
-
-      if (isThisParam) {
-        node.paramName = word.slice(1);
-      }
-
-      if (i === lastIndex) {
-        node.end = true;
-        node.route = route;
-      }
-
-      currentNode = node;
+  return routeSegments.reduce((acc, segment, i) => {
+    if (isThisParam(segment)) {
+      const key = segment.slice(1);
+      const value = pathSegments[i];
+      return [...acc, [key, value]];
     }
+    return acc;
+  }, []);
+};
+
+const serve = (routesTrie, path) => {
+  const pathSegments = path.split('/');
+  let currentNode = routesTrie;
+  let node = null;
+
+  for (const word of pathSegments) {
+    node = currentNode[word];
+
+    if (!node) {
+      if (currentNode['*']) {
+        node = currentNode['*'];
+      } else {
+        throw new Error('404 Not Found');
+      }
+    }
+    currentNode = node;
   }
 
-  return trieRoutePart;
+  if (!node.end) {
+    throw new Error('404 Not Found');
+  }
+  const { route } = node;
+
+  const params = getParamsByTemplate(path, route.path);
+  if (params.length) {
+    return { ...route, params: Object.fromEntries(params) };
+  }
+
+  return route.handler; // static
 };
 
 export default (routes) => {
   const routesTrie = generateTrie(routes);
 
   return {
-    serve(path) {
-      const pathSegments = path.split('/');
-      const params = [];
-      let currentNode = routesTrie;
-      let node = null;
-
-      for (const word of pathSegments) {
-        node = currentNode[word];
-
-        if (!node) {
-          if (currentNode['*']) {
-            node = currentNode['*'];
-            params.push([node.paramName, word]);
-          } else {
-            throw new Error('404 Not Found');
-          }
-        }
-        currentNode = node;
-      }
-
-      if (!node.end) {
-        throw new Error('404 Not Found');
-      }
-      const { route } = node;
-
-      if (params.length) {
-        return { ...route, params: Object.fromEntries(params) };
-      }
-      return route.handler; // static
-    },
+    serve: (path) => serve(routesTrie, path),
   };
 };
